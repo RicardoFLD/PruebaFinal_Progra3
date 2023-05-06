f35a2cb2e8e2875cd615571d0d10297dd11fa1a9
RicardoFLD <63625002+RicardoFLD@users.noreply.github.com>
2023-05-06 07:54:33 -0600

    -----

---
+++ BilleteraVirtual_v5/Models/Billetera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BilleteraVirtual_v5.Models
+{
+    public class Billetera
+    {
+        public string Foto { get; set; }
+        public string Banco { get; set; }
+        public string Emisor { get; set; }
+        public string Dueño { get; set; }
+        public int NumeroTarjeta { get; set; }
+
+        public int codigoCVV { get; set; }
+
+        public DateTime fechaExp { get; set; }
+
+    }
+}
